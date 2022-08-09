4674c9777280e21b7579ccaef17fa4c44a3184f3
Dane DeValcourt <dane.devalcourt@gmail.com>
2022-08-09 16:41:34 -0500

    Tip Calculator - Day 2

---
+++ day2/main.py
@@ -0,0 +1,13 @@
+# Tip Calculator
+
+print("Welcome to the tip calculator.")
+
+total = input("What was the total bill?")
+
+people = input("How many people to split the bill?")
+
+tip = input("What % tip would you like to give? 10, 15 or 20?")
+
+
+
+print("Each person should pay: " + ppperson)
