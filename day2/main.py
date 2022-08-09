44e7cdc0c753d4f25843adb3e2117b90656aee3d
Dane DeValcourt <dane.devalcourt@gmail.com>
2022-08-09 17:32:00 -0500

    Updating Day 2 Project

--- day2/main.py
+++ day2/main.py
@@ -8,6 +8,10 @@ people = input("How many people to split the bill?")
 
 tip = input("What % tip would you like to give? 10, 15 or 20?")
 
+tiptotal = int(total) * (float(tip) / 100)
+print (tiptotal)
+subtotal = int(tiptotal) + int(total)
+print (subtotal)
+ppperson = int(subtotal) / int(people)
 
-
-print("Each person should pay: " + ppperson)
+print("Each person should pay: " + str(float(ppperson)))
